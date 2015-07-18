9846210725ac7291bba0bf95e72bef65c6d7524e
Ashwin Tumma <ashwin.tumma23@gmail.com>
2015-07-18 08:49:07 -0700

    Tool for validation checking

--- run_product.sh
+++ run_product.sh
@@ -1,6 +1,9 @@
 #!/bin/bash +e
 
+# Run Java Application, and its JUnit Test Cases 
 cd java_src/Testng
-
 /home/ubuntu/jdk1.7.0_79/bin/javac -cp .:lib/* TestClass.java
 /home/ubuntu/jdk1.7.0_79/bin/java -cp .:lib/* org.junit.runner.JUnitCore TestClass 
+
+# Run tool for parsing the logs and in case of errors, log errors to Github Issues
+cd ../../validate_src/parseLogFiles.py > /dev/null
