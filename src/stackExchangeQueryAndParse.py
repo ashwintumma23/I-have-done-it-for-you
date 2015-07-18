9a485fc39258d8a1be1873dab09db134e384fcf5
Ashwin Tumma <ashwin.tumma23@gmail.com>
2015-07-18 04:21:04 -0700

    GitHub Issue creation and StackExchange Query

---
+++ src/stackExchangeQueryAndParse.py
@@ -0,0 +1,17 @@
+import json
+import requests 
+import urllib2 
+
+
+def queryStackExchange():
+	url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q=json%20file%20error&site=stackoverflow"
+	with requests.Session() as session:
+		js_response = session.get(url).json()
+		print js_response['items'][0]['link']
+		print js_response['items'][1]['link']
+		print js_response['items'][2]['link']
+		print js_response['items'][3]['link']
+		print js_response['items'][4]['link']
+		print js_response['items'][5]['link']
+
+queryStackExchange()
