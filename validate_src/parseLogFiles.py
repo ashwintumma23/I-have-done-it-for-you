f610db99c23a5aa5e3659679b8b226cf7bfe96ae
Ashwin Tumma <ashwin.tumma23@gmail.com>
2015-07-18 09:52:21 -0700

    Enhanced reporting

--- validate_src/parseLogFiles.py
+++ validate_src/parseLogFiles.py
@@ -3,10 +3,9 @@
 import requests 
 import os
 
+# Open java log file and parse till the exception is detected
 def parse_java_logs():
-	
 	currentJavaExceptions = []
-	# Open java log file and parse till the exception is detected
 	fjava = open("java_src/Testng/test.log","r")
 	for line in fjava:
 		if "Exception" in line:
@@ -18,18 +17,18 @@ def parse_java_logs():
 				currentJavaExceptions.append(exception_query_string)
 				stackExItems = queryStackExchange(exception_query_string)
 				createGitHubIssue(exception_query_string, stackExItems)
-				
 	fjava.close()
 
+# Open Python log file and parse till the exception is detected
 def parse_python_logs():
-	# Open Python log file and parse till the exception is detected
 	fpython = open("python_src/python_program_log.log","r")
 	for line in fpython:
 		if "Error" in line:
 			exception_query_string = line.strip()
 			stackExItems = queryStackExchange(exception_query_string)
 			createGitHubIssue(exception_query_string, stackExItems)
-				
+	fpython.close()	
+			
 def queryStackExchange(query):
 	stackExItems = []
         url = "https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&q="+query.strip()+"&site=stackoverflow"
@@ -44,7 +43,7 @@ def queryStackExchange(query):
 def createGitHubIssue(exception_query_string, stackExItems):
 	jenkins_build_number = os.environ.get('BUILD_NUMBER')
 	jenkins_build_url = os.environ.get('BUILD_URL')
-	curl_command = 'curl --user "ashwintumma23:APGA2dPD" -i -d \'{"title": "Build '+jenkins_build_number+' Error: '+exception_query_string+'","body": "The Jenkins build failed with the exception marked in the title. \\nView the complete error log at: [Jenkins Build '+jenkins_build_number+'](['+jenkins_build_url+'])\\n\\nWe have the following possible solutions on Stack Exchange which match the Exception.\\n'+str(stackExItems[0])+'\\n'+str(stackExItems[1])+'","labels": ["bug"]}\' https://api.github.com/repos/ashwintumma23/LinkedInHackDay/issues'
+	curl_command = 'curl --user "jenkinslinkedin15:APGA2dPD" -i -d \'{"title": "Build '+jenkins_build_number+' Error: '+exception_query_string+'","body": "The Jenkins build failed with the exception marked in the title. \\nView the complete error log at: [Jenkins Build '+jenkins_build_number+']('+jenkins_build_url+')\\n\\nWe have the following possible solutions on Stack Exchange which match the Exception.\\n'+str(stackExItems[0])+'\\n'+str(stackExItems[1])+'\\n'+str(stackExItems[2])+'\\n'+str(stackExItems[3])+'\\n","labels": ["bug"]}\' https://api.github.com/repos/ashwintumma23/LinkedInHackDay/issues'
 	os.system(curl_command)
 	print "Done Logging issue on Github Repository"
 
